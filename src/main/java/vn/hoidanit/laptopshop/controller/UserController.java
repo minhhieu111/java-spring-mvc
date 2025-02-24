cdd347c132e1b6da08ed4318400795f9e7aa2728
minhhieu111 <leminhhieu12112003@gmail.com>
2025-02-24 22:44:37 +0700

    video61

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,12 +1,13 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.hoidanit.laptopshop.domain.User;
-import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,8 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
+        List<User> arrUsers = this.userService.getAllUsersByEmail("Hieu@gmail.com");
+        System.out.println(arrUsers);
 
         model.addAttribute("eric", "test");
         model.addAttribute("hieu", "Form controller with model");
